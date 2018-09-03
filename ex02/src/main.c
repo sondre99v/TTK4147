ccd3d29d9fa13559543912e391653cd4c4d9b4a2
Edvard Narum <efnarum@stud.ntnu.no>
2018-09-03 14:29:44 +0200

    Complete task C

--- ex02/src/main.c
+++ ex02/src/main.c
@@ -1,12 +1,16 @@
 #include <stdio.h>
 #include <pthread.h>
+#include <semaphore.h>
 
 long global = 0;
+sem_t sem;
 
 void* fn(void* args) {
     long local = 0;
     for (int i = 0; i < 50e6; i++) {
+        sem_wait(&sem);
         global++;
+        sem_post(&sem);
         local++;
     }
     printf("Thread: %s, Global: %ld, Local: %ld\n",
@@ -16,6 +20,7 @@ void* fn(void* args) {
 }
 
 int main(int argc, char* argv[]) {
+    sem_init(&sem, 0, 1);
     pthread_t threadHandle1;
     pthread_t threadHandle2;
     pthread_create(&threadHandle1, NULL, fn, "1");
