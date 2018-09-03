6c2a8e3bc5afef3df14b4cdbd6ea26e5d2f9d59e
Edvard Narum <efnarum@stud.ntnu.no>
2018-09-03 16:02:14 +0200

    Finish task F

--- ex02/src/main.c
+++ ex02/src/main.c
@@ -3,10 +3,12 @@
 #include <pthread.h>
 #include <semaphore.h>
 
-#define N 6
+#define N 7
+
+const char* names[N] = { "Mathisen", "Reklev", "Petersen", "Henriksen", "Pedersen", "Lofstad", "Lervik" };
 
 static inline void eatSlowly(void){
-    for(long i = 0; i < 10000000; i++){
+    for(long i = 0; i < 100000000; i++){
         // "Memory clobber" - tells the compiler optimizer that all the memory 
         // is being touched, and that therefore the loop cannot be optimized out
         asm volatile("" ::: "memory");
@@ -14,15 +16,22 @@ static inline void eatSlowly(void){
 }
 
 pthread_mutex_t chpstcks[N];
+pthread_mutex_t pickupMutex;
 
 void* fn(void* args) {
     int n = (int)args;
 
-    for(;;) {
-        pthread_mutex_lock(&chpstcks[n]);
-        pthread_mutex_lock(&chpstcks[(n+1)%N]);
+    int eaten = 0;
 
-        printf("%d eating...\n", n);
+    for(;;) {
+        pthread_mutex_lock(&pickupMutex);
+        {
+            pthread_mutex_lock(&chpstcks[n]);
+            pthread_mutex_lock(&chpstcks[(n+1)%N]);
+        }
+        pthread_mutex_unlock(&pickupMutex);
+
+        printf("(%d) %s eating...\n", eaten++, names[n]);
         eatSlowly();
 
         pthread_mutex_unlock(&chpstcks[n]);
@@ -33,7 +42,15 @@ void* fn(void* args) {
 }
 
 int main(int argc, char* argv[]) {
+    srand(time(NULL));
+
     pthread_t phils[N];
+    pthread_mutex_init(&pickupMutex, NULL);
+
+
+    for(int i = 0; i < N; i++) {
+        pthread_mutex_init(&chpstcks[i], NULL);
+    }
     
     for (int i = 0; i < N; i++) {
         pthread_create(&phils[i], NULL, fn, (void*)i);
