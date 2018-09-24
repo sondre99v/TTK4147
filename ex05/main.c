08ef4fb207eb84060ccea3e9beb09b9af5de5a3f
Edvard Narum <efnarum@stud.ntnu.no>
2018-09-24 13:40:26 +0200

    Add files for exercise 5

---
+++ ex05/main.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <pthread.h>
+#include <time.h>
+#include <sched.h>
+#include <unistd.h>
+
+#include "io.h"
+
+//Fix compile warnings
+//#include <sched.h>
+
+/// Assigning CPU core ///
+int set_cpu(int cpu_number){
+	cpu_set_t cpu;
+	CPU_ZERO(&cpu);
+	CPU_SET(cpu_number, &cpu);
+
+	return pthread_setaffinity_np(pthread_self(), sizeof(cpu_set_t), &cpu);
+}
+
+/// 'struct timespec' functions ///
+struct timespec timespec_normalized(time_t sec, long nsec){
+    while(nsec >= 1000000000){
+        nsec -= 1000000000;
+        ++sec;
+    }
+    while(nsec < 0){
+        nsec += 1000000000;
+        --sec;
+    }
+    return (struct timespec){sec, nsec};
+}
+
+struct timespec timespec_add(struct timespec lhs, struct timespec rhs){
+    return timespec_normalized(lhs.tv_sec + rhs.tv_sec, lhs.tv_nsec + rhs.tv_nsec);
+}
+
+int main() {
+    /// Periodic execution example ///
+
+    struct timespec waketime;
+    clock_gettime(CLOCK_REALTIME, &waketime);
+
+    struct timespec period = {.tv_sec = 0, .tv_nsec = 500*1000*1000};
+
+    while(1){
+        // do periodic work ...
+        
+        // sleep
+        waketime = timespec_add(waketime, period);
+        clock_nanosleep(CLOCK_REALTIME, TIMER_ABSTIME, &waketime, NULL);
+    }    
+}
